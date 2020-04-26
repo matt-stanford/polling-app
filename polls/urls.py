0a35fb9cd7a7a983e7e7a6cc6ad52c166eca23e1
matt-stanford <mjfstanford@gmail.com>
2020-04-26 14:15:40 +0100

    added question and choices models

--- polls/urls.py
+++ polls/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('polls', views.polls, name='polls'),
+    path('polls/<int:poll_id>', views.poll, name='poll'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from .models import Question, Choice
 
 def index(request):
     return render(request, 'pages/index.html', {})
 
 def polls(request):
-    return render(request, 'pages/polls.html', {})
+    polls = Question.objects.all
+    return render(request, 'pages/polls.html', {'polls': polls})
+
+
+def poll(request, poll_id):
+    poll = Question.objects.filter(pk=poll_id)
+    choices = Choice.objects.all
+    context = {
+        'poll': poll,
+        'choices': choices
+    }
+    return render(request, 'pages/poll.html', context)
