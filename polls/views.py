232367f34a0e89134638f5d6ff92c87f6090adcf
matt-stanford <mjfstanford@gmail.com>
2020-04-27 20:40:30 +0100

    need to fix vote view

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from .models import Question, Choice
 
 def index(request):
@@ -16,3 +19,16 @@ def poll(request, poll_id):
 def result(request, poll_id):
     poll = Question.objects.get(pk=poll_id)
     return render(request, 'pages/result.html', {'poll': poll})
+
+
+def vote(request, poll_id):
+    poll = get_object_or_404(Question, pk=poll_id)
+    try:
+        choice = poll.choice_set.get(pk=request.POST['choice'])
+    except (KeyError):
+        return render(request, 'pages/poll.html', {'poll': poll})
+    else:
+        choice.votes += 1
+        choice.save()
+        return HttpResponseRedirect(reverse('result', args=(poll.id,)))
+
